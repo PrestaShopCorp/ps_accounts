17c49877cbd4edf14d8d2cd6b2b4ed0049d50656
Laurynas Šedys <laurynas.sedys@invertus.eu>
2020-09-17 16:36:11 +0300

    Moved product data collection to data provider

--- classes/Decorator/ProductDecorator.php
+++ classes/Decorator/ProductDecorator.php
@@ -4,7 +4,11 @@ namespace PrestaShop\Module\PsAccounts\Decorator;
 
 use Context;
 use Image;
+use PrestaShop\Module\PsAccounts\Formatter\ArrayFormatter;
+use PrestaShop\Module\PsAccounts\Repository\ConfigurationRepository;
+use PrestaShop\Module\PsAccounts\Repository\ImageRepository;
 use PrestaShop\Module\PsAccounts\Repository\LanguageRepository;
+use PrestaShop\Module\PsAccounts\Repository\ProductRepository;
 use Product;
 
 class ProductDecorator
@@ -17,11 +21,37 @@ class ProductDecorator
      * @var LanguageRepository
      */
     private $languageRepository;
+    /**
+     * @var ProductRepository
+     */
+    private $productRepository;
+    /**
+     * @var ArrayFormatter
+     */
+    private $arrayFormatter;
+    /**
+     * @var ConfigurationRepository
+     */
+    private $configurationRepository;
+    /**
+     * @var ImageRepository
+     */
+    private $imageRepository;
 
-    public function __construct(Context $context, LanguageRepository $languageRepository)
-    {
+    public function __construct(
+        Context $context,
+        LanguageRepository $languageRepository,
+        ProductRepository $productRepository,
+        ArrayFormatter $arrayFormatter,
+        ConfigurationRepository $configurationRepository,
+        ImageRepository $imageRepository
+    ) {
         $this->context = $context;
         $this->languageRepository = $languageRepository;
+        $this->productRepository = $productRepository;
+        $this->arrayFormatter = $arrayFormatter;
+        $this->configurationRepository = $configurationRepository;
+        $this->imageRepository = $imageRepository;
     }
 
     /**
@@ -31,6 +61,8 @@ class ProductDecorator
      */
     public function decorateProducts(array &$products)
     {
+        $weightUnit = $this->configurationRepository->get('PS_WEIGHT_UNIT');
+
         foreach ($products as &$product) {
             $productObject = new Product(
                 $product['id_product'],
@@ -42,9 +74,18 @@ class ProductDecorator
 
             $this->addLink($product);
             $this->addCoverImageLink($product);
+            $this->addProductImageLinks($product);
+            $this->addProductAttributes($product);
+            $this->addProductFeatures($product);
+            $this->formatProductWeight($product, $weightUnit);
         }
     }
 
+    /**
+     * @param array $product
+     *
+     * @return void
+     */
     private function addLink(array &$product)
     {
         $product['link'] = $this->context->link->getProductLink(
@@ -58,20 +99,72 @@ class ProductDecorator
         );
     }
 
+    /**
+     * @param array $product
+     *
+     * @return void
+     */
     private function addCoverImageLink(array &$product)
     {
-        $cover = Image::getCover($product['id_product']);
-        $product['cover'] =
-            ($cover !== false && is_array($cover)) ? $this->context->link->getImageLink($product['link_rewrite'], $cover['id_image'], 'home_default') : '';
+        $cover = $this->imageRepository->getProductCoverImage($product['id_product'], $this->context->shop->id);
+
+        $product['cover'] = is_string($cover) ?
+            $this->context->link->getImageLink($product['link_rewrite'], $cover, 'home_default') :
+            '';
     }
 
-    private function addProductAttributes()
+    /**
+     * @param array $product
+     *
+     * @return void
+     */
+    private function addProductAttributes(array &$product)
     {
-
+        if ((int) $product['id_attribute'] === 0) {
+            $product['attributes'] = '';
+        } else {
+            $product['attributes'] = $this->arrayFormatter->formatValueArray(
+                $this->productRepository->getAttributes((int) $product['id_attribute'], $product['iso_code'])
+            );
+        }
     }
 
-    private function addProductFeatures()
+    /**
+     * @param array $product
+     * @return void
+     */
+    private function addProductFeatures(array &$product)
     {
+        $product['features'] = $this->arrayFormatter->formatValueArray(
+            $this->productRepository->getFeatures((int) $product['id_product'], $product['iso_code'])
+        );
+    }
 
+    /**
+     * @param array $product
+     * @param string $weightUnit
+     * @return void
+     */
+    private function formatProductWeight(&$product, $weightUnit)
+    {
+        $product['weight'] = \Tools::ps_round((float) $product['weight'], 2) . " $weightUnit";
+    }
+
+    /**
+     * @param array $product
+     * @return void
+     */
+    private function addProductImageLinks(&$product)
+    {
+        $product['images'] = $this->arrayFormatter->formatArray(
+            array_map(function ($image) use ($product) {
+                return $this->context->link->getImageLink($product['link_rewrite'], $image['id_image'], 'home_default');
+            }, $this->imageRepository->getProductImages(
+                $product['id_product'],
+                $product['id_attribute'],
+                $this->context->shop->id
+            )
+            )
+        );
     }
 }
---
+++ classes/Provider/ProductDataProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace PrestaShop\Module\PsAccounts\Provider;
+
+use PrestaShop\Module\PsAccounts\Decorator\ProductDecorator;
+use PrestaShop\Module\PsAccounts\Repository\PaginatedApiDataProviderInterface;
+use PrestaShop\Module\PsAccounts\Repository\ProductRepository;
+
+class ProductDataProvider implements PaginatedApiDataProviderInterface
+{
+    /**
+     * @var ProductRepository
+     */
+    private $productRepository;
+    /**
+     * @var ProductDecorator
+     */
+    private $productDecorator;
+
+    public function __construct(ProductRepository $productRepository, ProductDecorator $productDecorator)
+    {
+        $this->productRepository = $productRepository;
+        $this->productDecorator = $productDecorator;
+    }
+
+    public function getFormattedData($offset, $limit)
+    {
+        $products = $this->productRepository->getProducts($offset, $limit);
+
+        $this->productDecorator->decorateProducts($products);
+
+        return array_map(function ($product) {
+            return [
+                'id' => "{$product['id_product']}-{$product['id_attribute']}-{$product['iso_code']}",
+                'collection' => 'products',
+                'properties' => $product,
+            ];
+        }, $products);
+    }
+
+    public function getRemainingObjectsCount($offset)
+    {
+        return 0;
+        // TODO: Implement getRemainingObjectsCount() method.
+    }
+}
