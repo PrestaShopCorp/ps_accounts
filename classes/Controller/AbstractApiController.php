72ac99a16f8159ebaba90494c9a55fd47afe75f9
Laurynas <laurynas.sedys@invertus.eu>
2020-12-02 18:23:17 +0200

    Added response status text from proxy response

--- classes/Controller/AbstractApiController.php
+++ classes/Controller/AbstractApiController.php
@@ -236,6 +236,8 @@ abstract class AbstractApiController extends ModuleFrontController
 
         if (array_key_exists((int) $code, Config::HTTP_STATUS_MESSAGES)) {
             $httpStatusText .= ' ' . Config::HTTP_STATUS_MESSAGES[(int) $code];
+        } elseif (isset($response['body']['status'])) {
+            $httpStatusText .= ' ' . $response['body']['status'];
         }
 
         header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
