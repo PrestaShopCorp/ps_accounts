c6f5c65b73b38afb51b6518c3938e821862b2fd4
Laurynas <laurynas.sedys@invertus.eu>
2021-02-10 20:46:29 +0200

    Added create table to upgrade

--- upgrade/upgrade-3.2.0.php
+++ upgrade/upgrade-3.2.0.php
@@ -21,6 +21,42 @@ function upgrade_module_3_2_0()
 
     $sqlQueries = [];
 
+    $sqlQueries[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'accounts_type_sync`
+    (
+        `type`               VARCHAR(50)      NOT NULL,
+        `offset`             INT(10) UNSIGNED NOT NULL DEFAULT 0,
+        `id_shop`            INT(10) UNSIGNED NOT NULL,
+        `lang_iso`           VARCHAR(3),
+        `full_sync_finished` TINYINT(1)       NOT NULL DEFAULT 0,
+        `last_sync_date`     DATETIME         NOT NULL
+    ) ENGINE = ENGINE_TYPE
+      DEFAULT CHARSET = utf8;';
+    $sqlQueries[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'accounts_sync`
+    (
+        `job_id`     VARCHAR(200) NOT NULL,
+        `created_at` DATETIME     NOT NULL
+    ) ENGINE = ENGINE_TYPE
+      DEFAULT CHARSET = utf8;';
+    $sqlQueries[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'accounts_deleted_objects`
+    (
+        `type`       VARCHAR(50)      NOT NULL,
+        `id_object`  INT(10) UNSIGNED NOT NULL,
+        `id_shop`    INT(10) UNSIGNED NOT NULL,
+        `created_at` DATETIME         NOT NULL,
+        PRIMARY KEY (`type`, `id_object`, `id_shop`)
+    ) ENGINE = ENGINE_TYPE
+      DEFAULT CHARSET = utf8;';
+    $sqlQueries[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'accounts_incremental_sync`
+    (
+        `type`       VARCHAR(50)      NOT NULL,
+        `id_object`  INT(10) UNSIGNED NOT NULL,
+        `id_shop`    INT(10) UNSIGNED NOT NULL,
+        `lang_iso`   VARCHAR(3),
+        `created_at` DATETIME         NOT NULL,
+        PRIMARY KEY (`type`, `id_object`, `id_shop`, `lang_iso`)
+    ) ENGINE = ENGINE_TYPE
+      DEFAULT CHARSET = utf8;';
+
     $sqlQueries[] = 'DELETE FROM `' . _DB_PREFIX_ . 'accounts_sync`;';
     $sqlQueries[] = 'ALTER TABLE `' . _DB_PREFIX_ . 'accounts_sync` CHANGE `job_id` `job_id` VARCHAR(200) NOT NULL;';
     $sqlQueries[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'accounts_deleted_objects` (
