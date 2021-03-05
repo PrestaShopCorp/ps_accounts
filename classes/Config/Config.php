603cc6bad0f28b5abe6a59934587697c66a4467b
Laurynas <laurynas.sedys@invertus.eu>
2021-03-05 16:34:59 +0200

    Added new check of limit parameter and new error code

--- classes/Config/Config.php
+++ classes/Config/Config.php
@@ -9,6 +9,7 @@ class Config
     const DATABASE_QUERY_ERROR_CODE = 454;
     const DATABASE_INSERT_ERROR_CODE = 455;
     const PS_FACEBOOK_NOT_INSTALLED = 456;
+    const INVALID_URL_QUERY = 458;
 
     const HTTP_STATUS_MESSAGES = [
         self::REFRESH_TOKEN_ERROR_CODE => 'Cannot refresh token',
@@ -16,5 +17,6 @@ class Config
         self::DATABASE_QUERY_ERROR_CODE => 'Database syntax error',
         self::DATABASE_INSERT_ERROR_CODE => 'Failed to write to database',
         self::PS_FACEBOOK_NOT_INSTALLED => 'Cannot sync Taxonomies without Facebook module',
+        self::INVALID_URL_QUERY => 'Invalid URL query'
     ];
 }
