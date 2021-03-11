8d6b2689cce8322aa6acd20780141b67ff3c58e4
Hervé Schoenenberger <herve.schoenenberger@prestashop.com>
2021-03-11 11:20:20 +0100

    store HMAC draft

--- controllers/front/apiV1ShopHmac.php
+++ controllers/front/apiV1ShopHmac.php
@@ -1,6 +1,7 @@
 <?php
 
 use PrestaShop\Module\PsAccounts\Controller\AbstractRestController;
+use PrestaShop\Module\PsAccounts\Service\ShopLinkAccountService;
 
 class ps_AccountsApiV1ShopHmacModuleFrontController extends AbstractRestController
 {
@@ -13,9 +14,20 @@ class ps_AccountsApiV1ShopHmacModuleFrontController extends AbstractRestControll
      */
     public function store(array $payload)
     {
+        /** @var ShopLinkAccountService $shopLinkAccountService */
+        $shopLinkAccountService = $this->module->getService(ShopLinkAccountService::class);
+
+        $shopLinkAccountService->writeHmac(
+            $payload['hmac'],
+            // FIXME : extraire l'id de la shop en cours
+            $payload['id'],
+            _PS_ROOT_DIR_ . '/upload/'
+        );
+
         return [
             'success' => true,
             'message' => 'HMAC stored successfully',
+            'url' => '/upload/' . $payload['id'] . '.txt',
         ];
     }
 }
