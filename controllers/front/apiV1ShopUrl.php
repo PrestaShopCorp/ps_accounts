d633e0a2e3d20c22a49f0034c2ab38d1361b7533
atourneriePresta <anthony.tournerie@prestashop.com>
2021-03-30 22:15:23 +0200

    feat: add ssl_activated to the return with the domains

--- controllers/front/apiV1ShopUrl.php
+++ controllers/front/apiV1ShopUrl.php
@@ -15,6 +15,22 @@ class ps_AccountsApiV1ShopUrlModuleFrontController extends AbstractShopRestContr
         return [
             'domain' => $shop->domain,
             'domain_ssl' => $shop->domain_ssl,
+            'ssl_activated' => $this->isSslActivated()
         ];
+    }
+
+    private function isSslActivated() {
+        // TODO It needs to be move to a different class
+        // Does a class already exist to get data from a shop?
+        $sslQuery = 'SELECT value
+                FROM ' . _DB_PREFIX_ . 'configuration
+                WHERE name = "PS_SSL_ENABLED_EVERYWHERE"
+        ';
+
+        $result = Db::getInstance()->executeS($sslQuery);
+        if (isset($result[0]) && isset($result[0]->value))
+            return $result[0]->value;
+
+        return 0;
     }
 }
