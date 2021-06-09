07fd917ab1fe397326898df29ae4e1e16c760f28
Hervé Schoenenberger <herve.schoenenberger@prestashop.com>
2021-06-09 17:59:50 +0200

    fix: safer sql request

--- classes/Repository/ConfigurationRepository.php
+++ classes/Repository/ConfigurationRepository.php
@@ -337,7 +337,7 @@ class ConfigurationRepository
     public function migrateToMultiShop(\Shop $shop)
     {
         \Db::getInstance()->query(
-            'UPDATE ' . _DB_PREFIX_ . "configuration SET id_shop=$shop->id, id_shop_group=$shop->id_shop_group"
+            'UPDATE ' . _DB_PREFIX_ . 'configuration SET id_shop=' . (int) $shop->id . ', id_shop_group=' . (int) $shop->id_shop_group
             . " WHERE (name like 'PS_ACCOUNTS_%' OR name IN ('PS_CHECKOUT_SHOP_UUID_V4', 'PSX_UUID_V4'))"
             . ' AND id_shop IS NULL AND id_shop_group IS NULL'
         );
@@ -355,7 +355,7 @@ class ConfigurationRepository
         \Db::getInstance()->query(
             'UPDATE ' . _DB_PREFIX_ . 'configuration SET id_shop=NULL, id_shop_group=NULL'
             . " WHERE (name like 'PS_ACCOUNTS_%' OR name IN ('PS_CHECKOUT_SHOP_UUID_V4', 'PSX_UUID_V4'))"
-            . " AND id_shop=$shop->id AND id_shop_group=$shop->id_shop_group"
+            . ' AND id_shop=' . (int) $shop->id . ' AND id_shop_group=' . (int) $shop->id_shop_group
         );
     }
 }
