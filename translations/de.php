a28b301011bd4b61caf93eb1459475af2a5b6b54
devops-prestashop <devops-prestashop@users.noreply.github.com>
2021-11-22 15:06:53 +0100

    [ci skip] fix: New (German) translations

--- translations/de.php
+++ translations/de.php
@@ -19,12 +19,12 @@
  */
 global $_MODULE;
 $_MODULE = [];
-$_MODULE['<{ps_accounts}prestashop>ps_accounts_c12a064dbfebf675a89d7c916258ee86'] = 'PrestaShop Account';
+$_MODULE['<{ps_accounts}prestashop>ps_accounts_c12a064dbfebf675a89d7c916258ee86'] = 'PrestaShop-Konto';
 $_MODULE['<{ps_accounts}prestashop>ps_accounts_89635180490c61910103399d55933738'] = 'Verknüpfen Sie Ihren Shop mit Ihrem PrestaShop-Konto, um Ihre Abonnemente in Ihrem Backoffice zu aktivieren und zu verwalten. Deinstallieren Sie dieses Modul nicht, solange Sie ein aktives Abonnement haben.';
 $_MODULE['<{ps_accounts}prestashop>ps_accounts_0ca78537b8a4a8fbe7e58b00bcaeaa69'] = 'Verknüpfen Sie Ihren Shop mit Ihrem PrestaShop-Konto, um Ihre Abonnemente in Ihrem Backoffice zu aktivieren und zu verwalten. Deinstallieren Sie dieses Modul nicht, solange Sie ein aktives Abonnement haben.';
-$_MODULE['<{ps_accounts}prestashop>ps_accounts_46c3a41d2feb9cd614cbc0606a91d472'] = 'This action will prevent immediately your PrestaShop services and Community services from working as they are using PrestaShop Accounts module for authentication.';
-$_MODULE['<{ps_accounts}prestashop>settingstranslations_3ae556b4e587c076e0a83035895a3023'] = 'Settings';
-$_MODULE['<{ps_accounts}prestashop>settingstranslations_19d7aeee5c0c2f5f35ac2e2ae188f5fb'] = 'Help';
+$_MODULE['<{ps_accounts}prestashop>ps_accounts_46c3a41d2feb9cd614cbc0606a91d472'] = 'Diese Aktion wird sofort verhindern, dass Ihre PrestaShop-Dienste und Community-Dienste zu arbeiten, da sie mit PrestaShop-Konten-Modul für die Authentifizierung sind.';
+$_MODULE['<{ps_accounts}prestashop>settingstranslations_3ae556b4e587c076e0a83035895a3023'] = 'Einstellungen';
+$_MODULE['<{ps_accounts}prestashop>settingstranslations_19d7aeee5c0c2f5f35ac2e2ae188f5fb'] = 'Hilfe';
 $_MODULE['<{ps_accounts}prestashop>settingstranslations_f98a72bcebc86d1cdb536d4f2321453d'] = 'Ihr Prestashop-Konto';
 $_MODULE['<{ps_accounts}prestashop>settingstranslations_3498492bdffcc04e709e154a74dd41d7'] = 'Ein einziges Konto zur Verwaltung aller Ihrer PrestaShop-Shops';
 $_MODULE['<{ps_accounts}prestashop>settingstranslations_77d3646d3307c9ec64022988a627e2c2'] = 'Erstellen Sie Ihr Prestashop-Konto oder melden Sie sich bei Ihrem bestehenden Konto an';
