8997535302dd9937afed3993e43cd1a92609280c
atourneriePresta <anthony.tournerie@prestashop.com>
2022-07-12 15:46:30 +0200

    feat: add the lib to sso (but we need to set the env)

--- _dev/src/zoid.ts
+++ _dev/src/zoid.ts
@@ -1,85 +1,59 @@
-import * as zoid from "zoid/dist/zoid";
 import axios from "axios";
+import { SignIn } from "@prestashopcorp/ps-accounts-sdk";
 
-// @ts-ignore
-// eslint-disable-next-line
-console.log(accountsUiUrl);
+const cdc = new SignIn({
+  onLogin: (idToken, refreshToken) => {
+    const redirect = document
+      .querySelector("input#redirect")
+      .getAttribute("value");
 
-const ssoComponent = zoid.create({
-  tag: "crossdomains-account-link-shop",
-  // @ts-ignore
-  // eslint-disable-next-line
-  url: ({ props }) => `${accountsUiUrl}/?cdc=true&workflow=sso`,
+    const params = new URLSearchParams();
+    params.append("ajax", "1");
+    params.append("token", idToken);
+    params.append("refreshToken", refreshToken);
+    params.append("provider", "ps_accounts");
+    params.append("submitLogin", "1");
+    params.append("redirect", redirect);
+    // @ts-ignore
+    // eslint-disable-next-line
+    params.append("stay_logged_in", $("#stay_logged_in:checked").val());
 
-  dimensions: {
-    width: "400px",
-    height: "600px",
+    // @ts-ignore
+    // eslint-disable-next-line
+    console.log(`id ${idToken} refresh ${refreshToken}`);
+    // index.php?controller=AdminAjaxPsAccounts&ajax=1&action=unlinkShop&token=' . Tools::getAdminTokenLite('AdminAjaxPsAccounts')
+    axios
+      .post(
+        `index.php?controller=AdminPsAccountsSsoConnect&ajax=1&action=ssoConnect`,
+        params,
+        {
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        if (res.data.hasErrors) {
+          // Function in prestashop core (global actually)
+          // @ts-ignore
+          // eslint-disable-next-line
+          displayErrors(res.data.errors);
+        } else {
+          window.location.assign(res.data.redirect);
+        }
+      })
+      .catch((err) => {
+        const error = document.querySelector("#error");
+        error.innerHTML = `<h3>TECHNICAL ERROR:</h3><p>Details: Error thrown: ${err.getMessage()}</p>`;
+        error.classList.toggle("hide");
+      });
   },
-
-  defaultContext: "popup",
-  context: "popup",
-
-  props: {
-    onLogin: {
-      type: "function",
-      required: false,
-    },
-  },
+  closeModalOnLogin: true,
+  env: "dev",
 });
 
-document
-  .querySelector(".prestashop-sso.picture")
-  .addEventListener("click", () => {
-    const sso = ssoComponent({
-      onLogin(idToken, refreshToken) {
-        const redirect = document
-          .querySelector("input#redirect")
-          .getAttribute("value");
-
-        sso.close();
-
-        const params = new URLSearchParams();
-        params.append("ajax", "1");
-        params.append("token", idToken);
-        params.append("refreshToken", refreshToken);
-        params.append("provider", "ps_accounts");
-        params.append("submitLogin", "1");
-        params.append("redirect", redirect);
-        // @ts-ignore
-        // eslint-disable-next-line
-        params.append("stay_logged_in", $("#stay_logged_in:checked").val());
-
-        // index.php?controller=AdminAjaxPsAccounts&ajax=1&action=unlinkShop&token=' . Tools::getAdminTokenLite('AdminAjaxPsAccounts')
-        axios
-          .post(
-            `index.php?controller=AdminPsAccountsSsoConnect&ajax=1&action=ssoConnect`,
-            params,
-            {
-              headers: {
-                Accept: "application/json",
-              },
-            }
-          )
-          .then((res) => {
-            if (res.data.hasErrors) {
-              // Function in prestashop core (global actually)
-              // @ts-ignore
-              // eslint-disable-next-line
-              displayErrors(res.data.errors);
-            } else {
-              window.location.assign(res.data.redirect);
-            }
-          })
-          .catch((err) => {
-            const error = document.querySelector("#error");
-            error.innerHTML = `<h3>TECHNICAL ERROR:</h3><p>Details: Error thrown: ${err.getMessage()}</p>`;
-            error.classList.toggle("hide");
-          });
-      },
-    });
-    sso.render();
-  });
+cdc.mount(".prestashop-sso.picture");
 
 export default {
-  ssoComponent,
+  cdc,
 };
