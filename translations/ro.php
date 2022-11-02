8e6812f468260d2c0fe75fbda72378bfa2cd36cf
devops-prestashop <devops-prestashop@users.noreply.github.com>
2022-11-02 13:49:09 +0100

    [ci skip] fix: New (Romanian) translations

---
+++ translations/ro.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+global $_MODULE;
+$_MODULE = [];
+$_MODULE['<{ps_accounts}prestashop>ps_accounts_40047b296fb8a4952e0512c7184e1ca3'] = 'PrestaShop Account';
+$_MODULE['<{ps_accounts}prestashop>ps_accounts_9006e8a0d8ca7f627146d32bc9c0eb73'] = 'Asociaza magazinul tău cu contul tău de PrestaShop pentru a activa si administra abonamentele tale in back office. Nu dezinstala acest modul dacă ai un abonament curent.';
+$_MODULE['<{ps_accounts}prestashop>ps_accounts_ceeadaf870d9eaa81cf1fbe32614af64'] = 'Această acțiune va împiedica imediat funcționarea serviciilor PrestaShop și a serviciilor comunitare, deoarece acestea utilizează modulul PrestaShop Account pentru autentificare.';
+$_MODULE['<{ps_accounts}prestashop>ps_accounts_01a84505e1f42ba1b80ffed4f9f21221'] = 'Acest magazin este legat de contul tău PrestaShop. Deconectează-ți magazinul dacă nu dorești să îți afectezi setările live.';
+$_MODULE['<{ps_accounts}prestashop>ps_accounts_d82864f800210969f401370615e30568'] = 'Unele magazine sunt legate de contul tău de PrestaShop. Ștergerea acestor magazine va afecta setările tale live.';
+$_MODULE['<{ps_accounts}prestashop>settingstranslations_f4f70727dc34561dfde1a3c529b6205c'] = 'Setări';
+$_MODULE['<{ps_accounts}prestashop>settingstranslations_6a26f548831e6a8c26bfbbd9f6ec61e0'] = 'Ajutor';
+$_MODULE['<{ps_accounts}prestashop>settingstranslations_b7ca62f12bc3b8e0d6422c028473a265'] = 'Contul tău PrestaShop';
+$_MODULE['<{ps_accounts}prestashop>settingstranslations_518d650937979216a269cf9830a7b484'] = 'Un singur cont pentru a-ți gestiona toate magazinele PrestaShop';
+$_MODULE['<{ps_accounts}prestashop>settingstranslations_655aa4af96bbb20cf78e14b560affb38'] = 'Creează contul de PrestaShop sau autentifică-te în contul tău existent';
+$_MODULE['<{ps_accounts}prestashop>settingstranslations_d23c41d498fe8587fb2f6ac563426dce'] = 'Asociază magazinul tău la cont';
+$_MODULE['<{ps_accounts}prestashop>content_b06f57f635db5e25514b7e637146608c'] = 'O interfață modulară';
+$_MODULE['<{ps_accounts}prestashop>content_768b56d98e614fd590f42ed1e867868d'] = 'Administrează-ți întreaga afacere într-un singur loc: catalogul de produse, comenzi, plată, livrare și multe altele.';
+$_MODULE['<{ps_accounts}prestashop>content_8ea4efcd82809db344ff0d7a76695448'] = 'Toate elementele esențiale pentru afacerea ta';
+$_MODULE['<{ps_accounts}prestashop>content_f27ec0e8d78513e2e60830c62a3a13b8'] = 'Marketing, plăți și analiza performanțelor: suita PrestaShop Essentials include toate caracteristicile de care ai nevoie pentru ca magazinul tău să aibă succes.';
+$_MODULE['<{ps_accounts}prestashop>content_f2d32f40a4060af2595051d0ab0278cb'] = 'O soluție 100% personalizabilă';
+$_MODULE['<{ps_accounts}prestashop>content_9b4e2d0b927094565d6ce673644868a4'] = 'PrestaShop îți însoțește creșterea. Găsești modulele noastre și ale partenerilor noștri pe PrestaShop Addons Marketplace pentru a personaliza și dezvolta magazinul tău';
+$_MODULE['<{ps_accounts}prestashop>content_1ab1f34b16d32fa54b90ee0f91f39dfd'] = 'Bun venit,';
+$_MODULE['<{ps_accounts}prestashop>content_474684646f833216fdaf79c606af208b'] = 'Accesează-ți Back Office pentru a gestiona magazinul tău.';
+$_MODULE['<{ps_accounts}prestashop>content_6ed6a381a1de805e2b807f88a95faa2b'] = 'Înapoi la back office';
+$_MODULE['<{ps_accounts}prestashop>content_1483546116fd85ebd65dcc595c69e73e'] = 'Conectează-te cu o altă metodă';
+$_MODULE['<{ps_accounts}prestashop>content_653f49dc0e643470e7b4ae9855974bbd'] = 'Adresa de e-mail nu este asociată unui cont PrestaShop backoffice. Pentru a afla mai multe, accesați [1]FAQ[/1]';
+$_MODULE['<{ps_accounts}prestashop>content_729f27762821b8ec88f1a656be7dff72'] = 'Trebuie să îți activezi contul mai întâi făcând clic pe link-ul din e-mail. Dacă ai nevoie să primești un nou link de activare,[1]te rugăm să apeși aici[/1]';
+$_MODULE['<{ps_accounts}prestashop>content_476ff05d1175f9a35d268828caf300f2'] = 'A apărut o eroare în timpul autentificării, te rugăm să contactezi suportul PrestaShop';
+$_MODULE['<{ps_accounts}prestashop>content_c8fc2e859e06da33e96d200b0106cfa3'] = 'Nu poți accesa back office-ul cu acest cont. Încearcă un alt cont sau contactează administratorul. [1]Mergi la Centrul de asistență[/1]';
