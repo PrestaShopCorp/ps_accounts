9e41ae3daaf50053ae7ac1ed126e272641dba253
devops-prestashop <devops-prestashop@users.noreply.github.com>
2022-11-07 14:39:21 +0100

    [ci skip] fix: New (Romanian) translations

---
+++ translations/ro.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+global $_MODULE;
+$_MODULE = [];
+$_MODULE['<{ps_accounts}prestashop>ps_accounts_40047b296fb8a4952e0512c7184e1ca3'] = 'PrestaShop Account';
+$_MODULE['<{ps_accounts}prestashop>ps_accounts_9006e8a0d8ca7f627146d32bc9c0eb73'] = 'Asociaza magazinul tău cu contul tău de PrestaShop pentru a activa si administra abonamentele tale in back office. Nu dezinstala acest modul dacă ai un abonament curent.';
+$_MODULE['<{ps_accounts}prestashop>ps_accounts_ceeadaf870d9eaa81cf1fbe32614af64'] = 'Această acțiune va împiedica imediat funcționarea serviciilor PrestaShop și a serviciilor comunitare, deoarece acestea utilizează modulul PrestaShop Account pentru autentificare.';
+$_MODULE['<{ps_accounts}prestashop>ps_accounts_01a84505e1f42ba1b80ffed4f9f21221'] = 'Acest magazin este legat de contul tău PrestaShop. Deconectează-ți magazinul dacă nu dorești să îți afectezi setările live.';
+$_MODULE['<{ps_accounts}prestashop>ps_accounts_d82864f800210969f401370615e30568'] = 'Unele magazine sunt legate de contul tău de PrestaShop. Ștergerea acestor magazine va afecta setările tale live.';
+$_MODULE['<{ps_accounts}prestashop>settingstranslations_f4f70727dc34561dfde1a3c529b6205c'] = 'Setări';
+$_MODULE['<{ps_accounts}prestashop>settingstranslations_6a26f548831e6a8c26bfbbd9f6ec61e0'] = 'Ajutor';
+$_MODULE['<{ps_accounts}prestashop>settingstranslations_b7ca62f12bc3b8e0d6422c028473a265'] = 'Contul tău PrestaShop';
+$_MODULE['<{ps_accounts}prestashop>settingstranslations_518d650937979216a269cf9830a7b484'] = 'Un singur cont pentru a-ți gestiona toate magazinele PrestaShop';
+$_MODULE['<{ps_accounts}prestashop>settingstranslations_655aa4af96bbb20cf78e14b560affb38'] = 'Creează contul de PrestaShop sau autentifică-te în contul tău existent';
+$_MODULE['<{ps_accounts}prestashop>settingstranslations_d23c41d498fe8587fb2f6ac563426dce'] = 'Asociază magazinul tău la cont';
