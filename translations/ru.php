2072f8183c147f90e8c10c6fc17fa4263635b4db
devops-prestashop <devops-prestashop@users.noreply.github.com>
2023-02-15 10:19:53 +0100

    [ci skip] fix: New (Russian) translations

--- translations/ru.php
+++ translations/ru.php
@@ -30,8 +30,8 @@ $_MODULE['<{ps_accounts}prestashop>settingstranslations_b7ca62f12bc3b8e0d6422c02
 $_MODULE['<{ps_accounts}prestashop>settingstranslations_518d650937979216a269cf9830a7b484'] = 'One account to manage all your PrestaShop shops';
 $_MODULE['<{ps_accounts}prestashop>settingstranslations_655aa4af96bbb20cf78e14b560affb38'] = 'Create your PrestaShop account or login to your existing account';
 $_MODULE['<{ps_accounts}prestashop>settingstranslations_d23c41d498fe8587fb2f6ac563426dce'] = 'Associate your shop with your account';
-$_MODULE['<{ps_accounts}prestashop>content_b06f57f635db5e25514b7e637146608c'] = 'A modular interface';
-$_MODULE['<{ps_accounts}prestashop>content_768b56d98e614fd590f42ed1e867868d'] = 'Manage your entire business in one place: product catalog, orders, payments, delivery and much more.';
+$_MODULE['<{ps_accounts}prestashop>content_23feb5280da1e3a86dc0fcc048f1a58d'] = 'A modular interface';
+$_MODULE['<{ps_accounts}prestashop>content_e661ffe463ad9b9bcea4c49d744058e7'] = 'Manage your entire business in one place: product catalog, orders, payments, delivery and much more.';
 $_MODULE['<{ps_accounts}prestashop>content_8ea4efcd82809db344ff0d7a76695448'] = 'All the essentials for your business';
 $_MODULE['<{ps_accounts}prestashop>content_f27ec0e8d78513e2e60830c62a3a13b8'] = 'Marketing, payment and performance analysis: the PrestaShop Essentials suite includes all the features you need to make your store successful.';
 $_MODULE['<{ps_accounts}prestashop>content_f2d32f40a4060af2595051d0ab0278cb'] = 'A 100% customizable solution';
