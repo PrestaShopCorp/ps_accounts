e8b89afc5f1dcbcd636cce6eb68adccf585cd54d
devops-prestashop <devops-prestashop@users.noreply.github.com>
2023-02-28 09:43:49 +0100

    [ci skip] fix: New (Polish) translations

--- translations/pl.php
+++ translations/pl.php
@@ -42,4 +42,4 @@ $_MODULE['<{ps_accounts}prestashop>content_6ed6a381a1de805e2b807f88a95faa2b'] = 
 $_MODULE['<{ps_accounts}prestashop>content_8905a5daaa6744846452108776efa036'] = 'Połącz się z innym urządzeniem';
 $_MODULE['<{ps_accounts}prestashop>content_729f27762821b8ec88f1a656be7dff72'] = 'Musisz najpierw aktywować swoje konto klikając na link w mailu. Jeśli chcesz otrzymać nowy link aktywacyjny,[1]kliknij tutaj[/1]';
 $_MODULE['<{ps_accounts}prestashop>content_476ff05d1175f9a35d268828caf300f2'] = 'Wystąpił błąd podczas logowania, prosimy o kontakt z supportem PrestaShop';
-$_MODULE['<{ps_accounts}prestashop>content_c8fc2e859e06da33e96d200b0106cfa3'] = 'You cannot access the back office with this account. Try another account or contact your administrator. [1]Go to Help Center[/1]';
+$_MODULE['<{ps_accounts}prestashop>content_fd359ea79740b865e45c2bf6fe5d3ab1'] = 'You cannot access the back office with this account. Try another account or contact your administrator.';
