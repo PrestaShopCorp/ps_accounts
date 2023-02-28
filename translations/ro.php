8e816b3589b4565ca0db6430217781a198d287f8
devops-prestashop <devops-prestashop@users.noreply.github.com>
2023-02-28 09:43:44 +0100

    [ci skip] fix: New (Romanian) translations

--- translations/ro.php
+++ translations/ro.php
@@ -42,4 +42,4 @@ $_MODULE['<{ps_accounts}prestashop>content_6ed6a381a1de805e2b807f88a95faa2b'] = 
 $_MODULE['<{ps_accounts}prestashop>content_8905a5daaa6744846452108776efa036'] = 'Conectează-te cu o altă metodă';
 $_MODULE['<{ps_accounts}prestashop>content_729f27762821b8ec88f1a656be7dff72'] = 'Trebuie să îți activezi contul mai întâi făcând clic pe link-ul din e-mail. Dacă ai nevoie să primești un nou link de activare,[1]te rugăm să apeși aici[/1]';
 $_MODULE['<{ps_accounts}prestashop>content_476ff05d1175f9a35d268828caf300f2'] = 'A apărut o eroare în timpul autentificării, te rugăm să contactezi suportul PrestaShop';
-$_MODULE['<{ps_accounts}prestashop>content_c8fc2e859e06da33e96d200b0106cfa3'] = 'Nu poți accesa back office-ul cu acest cont. Încearcă un alt cont sau contactează administratorul. [1]Mergi la Centrul de asistență[/1]';
+$_MODULE['<{ps_accounts}prestashop>content_fd359ea79740b865e45c2bf6fe5d3ab1'] = 'You cannot access the back office with this account. Try another account or contact your administrator.';
