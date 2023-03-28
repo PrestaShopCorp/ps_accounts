a935ed87c5273e30eda75003f66b8fee612d1599
Hervé Schoenenberger <herve.schoenenberger@prestashop.com>
2023-03-28 18:36:37 +0200

    feat: custom hooks for shop link & unlink events

--- controllers/front/apiV1ShopLinkAccount.php
+++ controllers/front/apiV1ShopLinkAccount.php
@@ -21,6 +21,7 @@
 use PrestaShop\Module\PsAccounts\Controller\AbstractShopRestController;
 use PrestaShop\Module\PsAccounts\DTO\Api\UpdateShopLinkAccountRequest;
 use PrestaShop\Module\PsAccounts\Exception\RefreshTokenException;
+use PrestaShop\Module\PsAccounts\Service\PsAccountsService;
 use PrestaShop\Module\PsAccounts\Service\ShopLinkAccountService;
 
 class ps_AccountsApiV1ShopLinkAccountModuleFrontController extends AbstractShopRestController
@@ -29,6 +30,11 @@ class ps_AccountsApiV1ShopLinkAccountModuleFrontController extends AbstractShopR
      * @var ShopLinkAccountService
      */
     private $shopLinkAccountService;
+
+    /**
+     * @var PsAccountsService
+     */
+    private $psAccountsService;
 
     /**
      * ps_AccountsApiV1ShopLinkAccountModuleFrontController constructor.
@@ -40,6 +46,8 @@ class ps_AccountsApiV1ShopLinkAccountModuleFrontController extends AbstractShopR
         parent::__construct();
 
         $this->shopLinkAccountService = $this->module->getService(ShopLinkAccountService::class);
+
+        $this->psAccountsService = $this->module->getService(PsAccountsService::class);
     }
 
     /**
@@ -57,6 +65,11 @@ class ps_AccountsApiV1ShopLinkAccountModuleFrontController extends AbstractShopR
             $this->module->getParameter('ps_accounts.verify_account_tokens')
         );
 
+        Hook::exec(Ps_accounts::HOOK_ACTION_SHOP_ACCOUNT_LINK_AFTER, [
+            'shopUuid' => $this->psAccountsService->getShopUuid(),
+            'shopId' => $shop->id,
+        ]);
+
         return [
             'success' => true,
             'message' => 'Link Account stored successfully',
@@ -71,7 +84,14 @@ class ps_AccountsApiV1ShopLinkAccountModuleFrontController extends AbstractShopR
      */
     public function delete(Shop $shop, array $payload): array
     {
+        $hookData = [
+            'shopUuid' => $this->psAccountsService->getShopUuid(),
+            'shopId' => $shop->id,
+        ];
+
         $this->shopLinkAccountService->resetLinkAccount();
+
+        Hook::exec(Ps_accounts::HOOK_ACTION_SHOP_ACCOUNT_UNLINK_AFTER, $hookData);
 
         return [
             'success' => true,
