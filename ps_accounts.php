4aa3d2e8da895bdbaf65ae8b9444d4c76733620a
Emmanuel Gautier <git@emmanuelgautier.fr>
2023-05-19 11:43:08 +0200

    fix: avoid open_basedir restriction

--- ps_accounts.php
+++ ps_accounts.php
@@ -664,7 +664,7 @@ class Ps_accounts extends Module
     {
         $this->loadAssets();
 
-        return $this->display(__FILE__, '/views/templates/admin/app.tpl');
+        return $this->display(__FILE__, 'views/templates/admin/app.tpl');
     }
 
     /**
