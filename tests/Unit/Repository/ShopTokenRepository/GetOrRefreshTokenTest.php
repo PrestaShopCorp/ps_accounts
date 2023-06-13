c3651c1f20e896b6e026f0413d74668189c9a79d
Hervé Schoenenberger <herve.schoenenberger@prestashop.com>
2023-06-13 18:17:53 +0200

    feat: update tests

--- tests/Unit/Repository/ShopTokenRepository/GetOrRefreshTokenTest.php
+++ tests/Unit/Repository/ShopTokenRepository/GetOrRefreshTokenTest.php
@@ -82,12 +82,6 @@ class GetOrRefreshTokenTest extends TestCase
         /** @var ConfigurationRepository $configuration */
         $configuration = $this->module->getService(ConfigurationRepository::class);
 
-//        /** @var ShopTokenRepository $tokenRepos */
-//        $tokenRepos = $this->getMockBuilder(ShopTokenRepository::class)
-//            ->setConstructorArgs([$configuration])
-//            ->setMethods(['client'])
-//            ->getMock();
-
         /** @var ShopTokenRepository $tokenRepos */
         $tokenRepos = $this->getMockBuilder(ShopTokenRepository::class)
             ->setConstructorArgs([$configuration])
