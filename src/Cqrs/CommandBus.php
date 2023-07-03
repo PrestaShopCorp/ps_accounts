47da66e11fea1964d5c4fb0cd1962d05746b38a8
Hervé Schoenenberger <herve.schoenenberger@prestashop.com>
2023-07-03 17:16:55 +0200

    refactor: abstract bus class

--- src/Cqrs/CommandBus.php
+++ src/Cqrs/CommandBus.php
@@ -2,18 +2,8 @@
 
 namespace PrestaShop\Module\PsAccounts\Cqrs;
 
-class CommandBus
+class CommandBus extends AbstractBus
 {
-    /**
-     * @var \Ps_accounts
-     */
-    protected $module;
-
-    public function __construct(\Ps_accounts $module)
-    {
-        $this->module = $module;
-    }
-
     /**
      * @param mixed $command
      *
@@ -21,41 +11,15 @@ class CommandBus
      *
      * @throws \Exception
      */
-    public function resolveHandler($command)
+    protected function resolveHandler($command)
     {
         $commandClass = get_class($command);
 
         $handlerClass = preg_replace(
-            '/((Command|Query)(\\\\[^\\\\]*$))/',
-            '${2}Handler${3}',
+            '/((Command)(\\\\[^\\\\]*$))/',
+            '${2}Handler${3}Handler',
             $commandClass, 1);
 
-        $handlerClass .= 'Handler';
-
         return $this->module->getService($handlerClass);
-    }
-
-    /**
-     * @param mixed $command
-     *
-     * @return mixed
-     *
-     * @throws \Exception
-     */
-    public function handle($command)
-    {
-        $this->module->getLogger()->debug('resolving handler : ' . get_class($command));
-
-        $handler = $this->resolveHandler($command);
-
-        if ($handler && method_exists($handler, 'handle')) {
-            /* @phpstan-ignore-next-line */
-            $this->module->getLogger()->debug('handling : ' . get_class($handler));
-            $this->module->getLogger()->debug('with data : ' . json_encode($command));
-
-            /* @phpstan-ignore-next-line */
-            return $handler->handle($command);
-        }
-        throw new \Exception('handle method not found');
     }
 }
--- src/Cqrs/QueryBus.php
+++ src/Cqrs/QueryBus.php
@@ -2,6 +2,24 @@
 
 namespace PrestaShop\Module\PsAccounts\Cqrs;
 
-class QueryBus extends CommandBus
+class QueryBus extends AbstractBus
 {
+    /**
+     * @param mixed $command
+     *
+     * @return mixed
+     *
+     * @throws \Exception
+     */
+    protected function resolveHandler($command)
+    {
+        $commandClass = get_class($command);
+
+        $handlerClass = preg_replace(
+            '/((Query)(\\\\[^\\\\]*$))/',
+            '${2}Handler${3}Handler',
+            $commandClass, 1);
+
+        return $this->module->getService($handlerClass);
+    }
 }
