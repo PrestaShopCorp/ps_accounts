b64db2bd13ff04c8f7539c12a3191f21760904f5
Hervé Schoenenberger <herve.schoenenberger@prestashop.com>
2023-07-04 11:08:37 +0200

    chore: cleanup uses

--- src/Cqrs/CommandBusTest.php
+++ src/Cqrs/CommandBusTest.php
@@ -2,14 +2,8 @@
 
 namespace PrestaShop\Module\PsAccounts\Cqrs;
 
-use PHPUnit\Framework\MockObject\MockObject;
 use PrestaShop\Module\PsAccounts\Domain\Account\Command\ForgetOauth2ClientCommand;
 use PrestaShop\Module\PsAccounts\Domain\Account\CommandHandler\ForgetOauth2ClientHandler;
-use PrestaShop\Module\PsAccounts\Domain\Shop\Command\LinkShopCommand;
-use PrestaShop\Module\PsAccounts\Domain\Shop\CommandHandler\LinkShopCommandHandler;
-use PrestaShop\Module\PsAccounts\Domain\Shop\Dto\LinkShop;
-use PrestaShop\Module\PsAccounts\Domain\Shop\Entity\OwnerSession;
-use PrestaShop\Module\PsAccounts\Domain\Shop\Entity\ShopSession;
 use PrestaShop\Module\PsAccounts\Tests\TestCase;
 
 class CommandBusTest extends TestCase
