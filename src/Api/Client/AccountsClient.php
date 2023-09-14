c9a5524c9a51a0598584f5b0a480a6d9a928213b
Antoine Metifeu <ametifeu@gmail.com>
2023-09-14 16:03:16 +0200

    fix: shop id passed as header value, not in header property of body

--- src/Api/Client/AccountsClient.php
+++ src/Api/Client/AccountsClient.php
@@ -93,10 +93,10 @@ class AccountsClient implements TokenClientInterface
         $this->getClient()->setRoute('shop/token/verify');
 
         return $this->getClient()->post([
+            'headers' => $this->getHeaders([
+                'X-Shop-Id' => $this->shopProvider->getShopContext()->getConfiguration()->getShopUuid(),
+            ]),
             'json' => [
-                'headers' => $this->getHeaders([
-                    'X-Shop-Id' => $this->shopProvider->getShopContext()->getConfiguration()->getShopUuid(),
-                ]),
                 'token' => $idToken,
             ],
         ]);
@@ -112,10 +112,10 @@ class AccountsClient implements TokenClientInterface
         $this->getClient()->setRoute('shop/token/refresh');
 
         return $this->getClient()->post([
+            'headers' => $this->getHeaders([
+                'X-Shop-Id' => $this->shopProvider->getShopContext()->getConfiguration()->getShopUuid(),
+            ]),
             'json' => [
-                'headers' => $this->getHeaders([
-                    'X-Shop-Id' => $this->shopProvider->getShopContext()->getConfiguration()->getShopUuid(),
-                ]),
                 'token' => $refreshToken,
             ],
         ]);
