54a211202fda37aa3c91661027c070145f25cb52
hschoenenberger <herve.schoenenberger@prestashop.com>
2024-07-08 09:30:53 +0200

    fix: more conditions before unlinking shop

--- src/Account/Session/ShopSession.php
+++ src/Account/Session/ShopSession.php
@@ -52,6 +52,11 @@ class ShopSession extends Session implements SessionInterface
     protected $oauth2ClientProvider;
 
     /**
+     * @var LinkShop
+     */
+    protected $linkShop;
+
+    /**
      * @param ConfigurationRepository $configurationRepository
      * @param ShopProvider $oauth2ClientProvider
      * @param CommandBus $commandBus
@@ -59,10 +64,12 @@ class ShopSession extends Session implements SessionInterface
     public function __construct(
         ConfigurationRepository $configurationRepository,
         ShopProvider $oauth2ClientProvider,
+        LinkShop $linkShop,
         CommandBus $commandBus
     ) {
         $this->configurationRepository = $configurationRepository;
         $this->oauth2ClientProvider = $oauth2ClientProvider;
+        $this->linkShop = $linkShop;
         $this->commandBus = $commandBus;
     }
 
@@ -88,7 +95,8 @@ class ShopSession extends Session implements SessionInterface
     public function refreshToken($refreshToken = null)
     {
         try {
-            if (!$this->oauth2ClientProvider->getOauth2Client()->exists()) {
+            if ($this->linkShop->exists() &&
+                !$this->oauth2ClientProvider->getOauth2Client()->exists()) {
                 $this->commandBus->handle(new UnlinkShopCommand($this->configurationRepository->getShopId()));
                 throw new RefreshTokenException('Invalid OAuth2 client');
             }
@@ -166,12 +174,6 @@ class ShopSession extends Session implements SessionInterface
      */
     private function getShopUuid()
     {
-        /** @var \Ps_accounts $module */
-        $module = \Module::getInstanceByName('ps_accounts');
-
-        /** @var LinkShop $linkShop */
-        $linkShop = $module->getService(LinkShop::class);
-
-        return $linkShop->getShopUuid();
+        return $this->linkShop->getShopUuid();
     }
 }
