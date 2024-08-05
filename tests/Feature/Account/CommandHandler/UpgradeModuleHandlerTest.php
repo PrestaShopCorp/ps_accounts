267a1e49d4700043d316372f978a66d4f6550291
hschoenenberger <herve.schoenenberger@prestashop.com>
2024-08-05 15:10:40 +0200

    fix: setUp parent method call

--- tests/Feature/Account/CommandHandler/UpgradeModuleHandlerTest.php
+++ tests/Feature/Account/CommandHandler/UpgradeModuleHandlerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PrestaShop\Module\PsAccounts\Tests\Feature\Api;
+namespace PrestaShop\Module\PsAccounts\Tests\Feature\Api\Account\CommandHandler;
 
 use PrestaShop\Module\PsAccounts\Adapter\ConfigurationKeys;
 use PrestaShop\Module\PsAccounts\Adapter\Link;
