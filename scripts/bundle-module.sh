2d1851dc6062674a9e95d65cdd7942bc3cabf7a2
Hervé Schoenenberger <54308193+hschoenenberger@users.noreply.github.com>
2024-10-31 16:06:00 +0100

    fix: php scoper mac (#445)
    * fix: mac friendly makefile
    
    ---------
    
    Co-authored-by: Sullivan <sullivan.monteiro@prestashop.com>

--- scripts/bundle-module.sh
+++ scripts/bundle-module.sh
@@ -23,7 +23,7 @@ echo "${filename} ..."
 
 rm -rf "./${dist}/${module}"
 mkdir -p "./${dist}/${module}"
-cp -pr -t "./${dist}/${module}" $(cat .zip-contents)
+cp -r $(cat .zip-contents) "./${dist}/${module}"
 WORKDIR="${dist}" make autoindex
 
 cd $dist || exit;
