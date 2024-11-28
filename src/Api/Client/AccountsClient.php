be9c383065799709d745915eb246cb35664aea06
hschoenenberger <herve.schoenenberger@prestashop.com>
2024-11-28 12:04:54 +0100

    feat: add x-multishop-enabled header

--- src/Api/Client/AccountsClient.php
+++ src/Api/Client/AccountsClient.php
@@ -90,6 +90,7 @@ class AccountsClient
             'Accept' => 'application/json',
             'X-Module-Version' => \Ps_accounts::VERSION,
             'X-Prestashop-Version' => _PS_VERSION_,
+            'X-Multishop-Enabled' => \Shop::isFeatureActive(),
             'X-Request-ID' => Uuid::uuid4()->toString(),
         ], $additionalHeaders);
     }
