189f098c5d246c57c38a193a40a3ae4b55bdfdf9
Hervé Schoenenberger <54308193+hschoenenberger@users.noreply.github.com>
2024-11-29 10:50:06 +0100

    [ACCOUNT-2732] refactor: replace module-lib-service-container (#447)
    * refactor: get rid of module-lib-service-container
    
    * refactor: remove useless deps
    
    * refactor: create provider files
    
    * chore: empty services files
    
    * chore: cleanup composer.json
    
    * fix: static closures
    
    * refactor: early logger injection
    
    * refactor: switch to the lightweight-container lib
    
    * feat: scoping lightweight-container

---
+++ config.prod.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+return [
+    'ps_accounts.environment' => 'production',
+    'ps_accounts.accounts_api_url' => 'https://accounts-api.distribution.prestashop.net/',
+    'ps_accounts.accounts_ui_url' => 'https://accounts.distribution.prestashop.net',
+    'ps_accounts.sso_api_url' => 'https://auth.prestashop.com/api/v1/',
+    'ps_accounts.sso_account_url' => 'https://auth.prestashop.com/login',
+    'ps_accounts.sso_resend_verification_email_url' => 'https://auth.prestashop.com/account/send-verification-email',
+    'ps_accounts.billing_api_url' => 'https://billing-api.distribution.prestashop.net/',
+    'ps_accounts.indirect_channel_api_url' => 'https://indirect-channel-api.prestashop.net',
+    'ps_accounts.sentry_credentials' => 'https://cd2a5f089edb6d6efe742c0cbe004106@o298402.ingest.us.sentry.io/5354585',
+    'ps_accounts.segment_write_key' => 'pEJGnRxw47CU01efFjMyl1S7YcxshLxl',
+    'ps_accounts.check_api_ssl_cert' => true,
+    'ps_accounts.verify_account_tokens' => true,
+    'ps_accounts.accounts_vue_cdn_url' => 'https://unpkg.com/prestashop_accounts_vue_components@3/dist/psaccountsVue.umd.min.js',
+    'ps_accounts.accounts_cdn_url' => 'https://unpkg.com/prestashop_accounts_vue_components@5',
+    'ps_accounts.svc_accounts_ui_url' => 'https://accounts.psessentials.net',
+    'ps_accounts.oauth2_url' => 'https://oauth.prestashop.com',
+
+    'ps_accounts.testimonials_url' => 'https://assets.prestashop3.com/dst/accounts/assets/testimonials.json',
+];
