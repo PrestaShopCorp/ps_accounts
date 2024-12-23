6d655150dd0e63acf5c85a9138d159e7eb679333
hschoenenberger <herve.schoenenberger@prestashop.com>
2024-12-23 18:40:57 +0100

    feat: phpstan

--- src/Api/Controller/AbstractV2RestController.php
+++ src/Api/Controller/AbstractV2RestController.php
@@ -146,7 +146,7 @@ abstract class AbstractV2RestController extends AbstractRestController
      *
      * @return mixed
      */
-    protected function extractMethod(array & $payload)
+    protected function extractMethod(array &$payload)
     {
         $method = $_SERVER['REQUEST_METHOD'];
         // detect method from payload (hack with some shop server configuration)
