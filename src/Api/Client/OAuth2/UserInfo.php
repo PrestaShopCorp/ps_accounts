ae365104015963b55b1896e8ea18cdae6bbf7abf
hschoenenberger <herve.schoenenberger@prestashop.com>
2025-01-16 18:09:45 +0100

    fix: php-cs-fixer

--- src/Api/Client/OAuth2/UserInfo.php
+++ src/Api/Client/OAuth2/UserInfo.php
@@ -18,7 +18,6 @@
  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
  */
 
-
 namespace PrestaShop\Module\PsAccounts\Api\Client\OAuth2;
 
 // [OAuth2] stdClass Object (
