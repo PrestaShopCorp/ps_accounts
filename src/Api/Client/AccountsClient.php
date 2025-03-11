6d273cd1397ebb70662edb3887499509a35d3f0b
hschoenenberger <herve.schoenenberger@prestashop.com>
2025-03-11 16:48:04 +0100

    chore: todo

--- src/Api/Client/AccountsClient.php
+++ src/Api/Client/AccountsClient.php
@@ -154,6 +154,7 @@ class AccountsClient
             'v1/user/' . $ownerUid . '/shop/' . $shopUid,
             [
                 Request::HEADERS => $this->getHeaders([
+                    # FIXME: use shop access token instead
                     'Authorization' => 'Bearer ' . $ownerToken,
                     'X-Shop-Id' => $shopUid,
                 ]),
