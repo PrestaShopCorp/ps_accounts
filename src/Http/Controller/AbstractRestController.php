602051ccc8c8214c00d082157a1cb7ef11780bfc
hschoenenberger <herve.schoenenberger@prestashop.com>
2025-04-08 10:32:29 +0200

    fix: getRequestHeader

--- src/Http/Controller/AbstractRestController.php
+++ src/Http/Controller/AbstractRestController.php
@@ -307,14 +307,36 @@ abstract class AbstractRestController extends ModuleFrontController
     /**
      * @param string $header
      *
-     * @return mixed|null
+     * @return string|null
      */
     protected function getRequestHeader($header)
     {
+        $headerValue = null;
+
         $headerKey = 'HTTP_' . strtoupper(str_replace('-', '_', $header));
 
         if (array_key_exists($headerKey, $_SERVER)) {
-            return $_SERVER[$headerKey];
+            $headerValue = $_SERVER[$headerKey];
+        }
+
+        if (null === $headerValue) {
+            $headerValue = $this->getApacheHeader($header);
+        }
+
+        return $headerValue;
+    }
+
+    /**
+     * @param string $header
+     *
+     * @return string|null
+     */
+    protected function getApacheHeader($header)
+    {
+        $headers = getallheaders();
+        //$header = preg_replace('/PrestaShop/', 'Prestashop', $header);
+        if (array_key_exists($header, $headers)) {
+            return $headers[$header];
         }
 
         return null;
