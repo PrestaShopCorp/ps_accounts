fe70a3b73ae3b3df51699d4a94f08a109efc7742
ervinpresta <ervin.klicic@prestashop.com>
2025-04-15 16:58:57 +0200

    add slack notifications

--- e2e-env/scripts/post-install-scripts/post-install.sh
+++ e2e-env/scripts/post-install-scripts/post-install.sh
@@ -22,6 +22,7 @@ if [ "$PHYSICAL_URI" ]; then
     -e "UPDATE ps_shop_url SET physical_uri='/$PHYSICAL_URI/'"
 fi
 
+#Install PS_ACCOUNTS
 set -eu
 cd "$(dirname $0)" || exit 1
 
@@ -40,6 +41,10 @@ TARGET_ASSET="ps_accounts_preprod-${CLEANED_VERSION#v}.zip"
 echo "* [ps_accounts] downloading..."
 echo "https://github.com/${GITHUB_REPOSITORY}/releases/download/${TARGET_VERSION}/${TARGET_ASSET}"
 wget -q -O /tmp/ps_accounts.zip "https://github.com/${GITHUB_REPOSITORY}/releases/download/${TARGET_VERSION}/${TARGET_ASSET}"
+
 echo "* [ps_accounts] unziping..."
-unzip -qq /tmp/ps_accounts.zip -d /var/www/html/modules
-echo "* [ps_accounts] installing the module..."
+unzip -qq /tmp/ps_accounts.zip -d /var/www/html/$PHYSICAL_URI/modules
+
+echo "* Fixing permissions..."
+chown -R www-data:www-data /var/www/html/$PHYSICAL_URI/modules/ps_accounts
+chmod -R 755 /var/www/html/$PHYSICAL_URI/modules/ps_accounts
