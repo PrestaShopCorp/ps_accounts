68fb5812da5fc9c97869981943d881e2e95c13e5
hschoenenberger <herve.schoenenberger@prestashop.com>
2025-04-24 09:07:59 +0200

    chore: activate display hook for alpha debugging

--- ps_accounts.php
+++ ps_accounts.php
@@ -106,7 +106,7 @@ class Ps_accounts extends Module
 
         // Login/Logout OAuth
         // PS 1.6 - 1.7
-        //'displayAdminAfterHeader',
+        'displayAdminAfterHeader',
         'actionAdminLoginControllerSetMedia',
         // PS >= 8
         //'actionAdminControllerInitBefore',
