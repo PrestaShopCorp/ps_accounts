97e2be9dcebe156a0513b44158436aa0a6b75da7
ervinpresta <ervin.klicic@prestashop.com>
2025-04-25 17:31:05 +0200

    change .env in CI

--- e2e/fixtures/goToModuleManagerPage.fixture.ts
+++ e2e/fixtures/goToModuleManagerPage.fixture.ts
@@ -5,7 +5,6 @@ import {PageManager} from '~/pages/managerPage';
 export const gotToModuleManagerPage = loginFixture.extend<MyFixtures>({
   gotToModuleManagerPage: async ({loginFixture, page}, use) => {
     const pm = new PageManager(loginFixture);
-    console.log(page.url());
     if (await pm.fromDashboardPage().getShopVersion()) {
       await pm.fromBasePage().goToModulesManagerOldPsVersion();
     } else {
