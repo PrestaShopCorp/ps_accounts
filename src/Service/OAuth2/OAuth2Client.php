c1ad2103da69a70976a6fedc6e4b694f5222f4a8
hschoenenberger <herve.schoenenberger@prestashop.com>
2025-05-12 17:35:16 +0200

    fix: getRedirectUri compat v9

--- src/Service/OAuth2/OAuth2Client.php
+++ src/Service/OAuth2/OAuth2Client.php
@@ -129,9 +129,9 @@ class OAuth2Client
     {
         if (defined('_PS_VERSION_')
             && version_compare(_PS_VERSION_, '9', '>=')) {
-            return $this->link->getAdminLink('SfAdminOAuth2PsAccounts', false, [
+            return $this->link->getAdminLink('SfAdminOAuth2PsAccounts', false, array_merge([
                 'route' => 'ps_accounts_oauth2',
-            ], $query, true);
+            ], $query), $query, true);
         }
 
         return $this->link->getAdminLink('AdminOAuth2PsAccounts', false, [], $query, true);
