020a1530d95707b1110ea72aef6f8a40c99e4ecf
hschoenenberger <herve.schoenenberger@prestashop.com>
2025-06-12 16:56:54 +0200

    chore: bump version number

--- ps_accounts.php
+++ ps_accounts.php
@@ -32,7 +32,7 @@ class Ps_accounts extends Module
 
     // Needed in order to retrieve the module version easier (in api call headers) than instanciate
     // the module each time to get the version
-    const VERSION = '7.2.0';
+    const VERSION = '7.2.1';
 
     /**
      * Admin tabs
@@ -129,7 +129,7 @@ class Ps_accounts extends Module
 
         // We cannot use the const VERSION because the const is not computed by addons marketplace
         // when the zip is uploaded
-        $this->version = '7.2.0';
+        $this->version = '7.2.1';
 
         $this->module_key = 'abf2cd758b4d629b2944d3922ef9db73';
 
