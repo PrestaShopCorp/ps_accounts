faf5c2d8fb465929bce5773040d7dc366f84cf0c
ervinpresta <ervin.klicic@prestashop.com>
2025-07-02 17:09:26 +0200

    add if condition in method uplaod zip for PS_Version 1.6

--- e2e/pages/moduleManager/moduleManagerPage.ts
+++ e2e/pages/moduleManager/moduleManagerPage.ts
@@ -103,13 +103,26 @@ export default class ModuleManagerPage extends BasePage {
    * Upload a zip
    */
   async uploadZip() {
-    await this.page.locator('[data-target="#module-modal-import"]').click();
-    const fileChooserPromise = this.page.waitForEvent('filechooser');
-    await this.page.locator('.module-import-start-select-manual').click();
-    const fileChooser = await fileChooserPromise;
-    await fileChooser.setFiles(path.join(__dirname, '../../../e2e-env/modules/ps_accounts_preprod-7.2.0.zip'));
-    await this.page.waitForSelector('.module-import-success-icon');
-    await this.page.locator('#module-modal-import-closing-cross').click();
-    await this.page.reload({waitUntil: 'commit'});
+    const pageTitle = await this.getPageMainTitle();
+    const pageTitleOldPsVersion = await this.getPageMainTitleOldPsVersion();
+    if (pageTitle === moduleManagerPagesLocales.moduleManager.en_EN.title) {
+      await this.page.locator('[data-target="#module-modal-import"]').click();
+      const fileChooserPromise = this.page.waitForEvent('filechooser');
+      await this.page.locator('.module-import-start-select-manual').click();
+      const fileChooser = await fileChooserPromise;
+      await fileChooser.setFiles(path.join(__dirname, '../../../e2e-env/modules/ps_accounts_preprod-7.2.0.zip'));
+      await this.page.waitForSelector('.module-import-success-icon');
+      await this.page.locator('#module-modal-import-closing-cross').click();
+      await this.page.reload({waitUntil: 'commit'});
+    } else if (pageTitleOldPsVersion === moduleManagerPagesLocales.moduleManager.en_EN.titleOldPsVersion) {
+      await this.page.locator('#desc-module-new').click();
+      const fileChooserPromise = this.page.waitForEvent('filechooser');
+      await this.page.locator('#file-selectbutton').click();
+      const fileChooser = await fileChooserPromise;
+      await fileChooser.setFiles(path.join(__dirname, '../../../e2e-env/modules/ps_accounts_preprod-7.2.0.zip'));
+      await this.page.locator('[name="download"]').click();
+      await this.page.waitForSelector('.alert.alert-success');
+      await this.page.reload({waitUntil: 'commit'});
+    }
   }
 }
