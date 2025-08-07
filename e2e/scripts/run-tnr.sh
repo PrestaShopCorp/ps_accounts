c19e157c1b2472c8c69257a3337d74ca249978da
ervinpresta <ervin.klicic@prestashop.com>
2025-08-07 18:24:52 +0200

    change run-tnr.sh

--- e2e/scripts/run-tnr.sh
+++ e2e/scripts/run-tnr.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-if [ -n "$1" ]; then
-  SHOP_VERSIONS=("$1")
-else
-SHOP_VERSIONS=(
-  # nightly-nginx    
-  # 8.2.0-8.1-fpm-alpine
-  # 1.7.8.8-7.4-fpm-alpine
-  # 1.6.1.24-7.1-fpm-alpine
-)
-fi
-
-for PS_VERSION in "${SHOP_VERSIONS[@]}"; do
 #Build the shop 
 npm run build-shop -- "$PS_VERSION"
 
