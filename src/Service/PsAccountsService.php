55fa5ccd13f619fd25d193a6a35cc42340dcedb1
Antoine Metifeu <ametifeu@gmail.com>
2025-08-12 14:13:35 +0200

    fix: remove token on urls

--- src/Service/PsAccountsService.php
+++ src/Service/PsAccountsService.php
@@ -277,7 +277,7 @@ class PsAccountsService
      */
     public function getContextUrl()
     {
-        return $this->link->getAdminLink('AdminAjaxV2PsAccounts', true, [], ['ajax' => 1, 'action' => 'getContext']);
+        return $this->link->getAdminLink('AdminAjaxV2PsAccounts', false, [], ['ajax' => 1, 'action' => 'getContext']);
     }
 
     /**
