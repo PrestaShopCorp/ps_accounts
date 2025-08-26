cb0aaf74e1dbe10bce99c51b52bfd2103947a3dc
hschoenenberger <herve.schoenenberger@prestashop.com>
2025-08-26 15:57:12 +0200

    feat: example of hook injecting alert with js

--- ps_accounts.php
+++ ps_accounts.php
@@ -107,7 +107,7 @@ class Ps_accounts extends Module
         // PS 1.6 - 1.7
         'displayAdminAfterHeader',  // FIXME: for alpha version only
         'actionAdminLoginControllerSetMedia',
-        'actionAdminControllerSetMedia'
+        'actionAdminControllerSetMedia',
         // PS >= 8
         //'actionAdminControllerInitBefore',
     ];
