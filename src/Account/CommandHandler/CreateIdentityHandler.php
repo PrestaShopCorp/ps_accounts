196aadbf40ccacad3ebf681968d141d872055d3b
Guillaume Lepoetre <guillaume.lepoetre@prestashop.com>
2025-09-04 14:30:24 +0200

    feat: add isVerified status management in CreateIdentityHandler and StatusManager

--- src/Account/CommandHandler/CreateIdentityHandler.php
+++ src/Account/CommandHandler/CreateIdentityHandler.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright since 2007 PrestaShop SA and Contributors
  * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
@@ -104,6 +105,7 @@ class CreateIdentityHandler
                 $identityCreated->clientSecret
             );
             $this->statusManager->setCloudShopId($identityCreated->cloudShopId);
+            $this->statusManager->setIsVerified(false);
             $this->statusManager->invalidateCache();
         }
         $this->commandBus->handle(new VerifyIdentityCommand(
