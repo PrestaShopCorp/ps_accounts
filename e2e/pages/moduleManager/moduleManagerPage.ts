aa005a96064c1dc18479237aa0e66f1189b05378
ervinpresta <ervin.klicic@prestashop.com>
2025-10-13 15:06:17 +0200

    🏗️ change uploadzip function

--- e2e/pages/moduleManager/moduleManagerPage.ts
+++ e2e/pages/moduleManager/moduleManagerPage.ts
@@ -115,7 +115,7 @@ export default class ModuleManagerPage extends BasePage {
       const fileChooserPromise = this.page.waitForEvent('filechooser');
       await this.page.locator('.module-import-start-select-manual').click();
       const fileChooser = await fileChooserPromise;
-      await fileChooser.setFiles(path.join(__dirname, '../../../e2e-env/modules/ps_accounts_preprod-7.2.0.zip'));
+      await fileChooser.setFiles(path.join(__dirname, '../../../e2e-env/modules/ps_accounts_preprod-8.0.2.zip'));
       await this.page.waitForSelector('.module-import-success-icon');
       await this.page.locator('#module-modal-import-closing-cross').click();
       await this.page.reload({waitUntil: 'commit'});
@@ -124,7 +124,7 @@ export default class ModuleManagerPage extends BasePage {
       const fileChooserPromise = this.page.waitForEvent('filechooser');
       await this.page.locator('#file-selectbutton').click();
       const fileChooser = await fileChooserPromise;
-      await fileChooser.setFiles(path.join(__dirname, '../../../e2e-env/modules/ps_accounts_preprod-7.2.0.zip'));
+      await fileChooser.setFiles(path.join(__dirname, '../../../e2e-env/modules/ps_accounts_preprod-8.0.2.zip'));
       await this.page.locator('[name="download"]').click();
       await this.page.waitForSelector('.alert.alert-success');
       await this.page.reload({waitUntil: 'commit'});
