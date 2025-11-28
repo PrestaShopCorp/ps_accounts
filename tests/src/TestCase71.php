44a58c6ee73cc0fba503b41e3c282b65a26b1ad3
Sullivan <sullivan.monteiro@prestashop.com>
2025-11-28 15:30:12 +0100

    fix: remove assertStringContainsString

--- tests/src/TestCase71.php
+++ tests/src/TestCase71.php
@@ -15,24 +15,4 @@ class TestCase71 extends BaseTestCase
     {
         $this->tear_down();
     }
-
-    /**
-     * @param string $needle
-     * @param string $haystack
-     * @param string $message
-     *
-     * @return void
-     */
-    public static function assertStringContainsString($needle, $haystack, $message = '')
-    {
-        if (method_exists(\PHPUnit\Framework\Assert::class, 'assertStringContainsString')) {
-            \PHPUnit\Framework\Assert::assertStringContainsString($needle, $haystack, $message);
-        } else {
-            // Fallback for PHPUnit < 7.5: use strpos to check if needle is in haystack
-            if ($message === '') {
-                $message = sprintf('Failed asserting that \'%s\' contains \'%s\'.', $haystack, $needle);
-            }
-            self::assertNotFalse(strpos($haystack, $needle), $message);
-        }
-    }
 }
