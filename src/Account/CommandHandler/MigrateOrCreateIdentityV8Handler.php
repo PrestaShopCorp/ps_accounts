d9d5c694787811d3f0a9244c5168cf31fc98f6a8
hschoenenberger <herve.schoenenberger@prestashop.com>
2025-12-19 15:53:51 +0100

    fix: add a check to avoid creating a new identity when,  forcing migration from advanced settings form

--- src/Account/CommandHandler/MigrateOrCreateIdentityV8Handler.php
+++ src/Account/CommandHandler/MigrateOrCreateIdentityV8Handler.php
@@ -161,7 +161,9 @@ class MigrateOrCreateIdentityV8Handler
             $this->statusManager->invalidateCache();
             $this->registerLatestVersion();
         } catch (AccountsException $e) {
-            if ($e->getErrorCode() === AccountsException::ERROR_STORE_LEGACY_NOT_FOUND) {
+            if ($e->getErrorCode() === AccountsException::ERROR_STORE_LEGACY_NOT_FOUND &&
+                $command->origin != AccountsService::ORIGIN_ADVANCED_SETTINGS
+            ) {
                 $this->registerLatestVersion();
                 $this->cleanupIdentity();
                 $this->createOrVerifyIdentity($command);
