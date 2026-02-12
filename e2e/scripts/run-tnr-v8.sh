75da4abd22a671c424b4187ca752d1e05b7ade50
ervinpresta <ervin.klicic@prestashop.com>
2026-02-12 11:02:12 +0100

    incresase sleep

--- e2e/scripts/run-tnr-v8.sh
+++ e2e/scripts/run-tnr-v8.sh
@@ -16,7 +16,7 @@ for PS_VERSION in "${SHOP_VERSIONS[@]}"; do
 #Build the shop 
 sleep 4
 npm run build-shop -- "$PS_VERSION" "" "imageoff" "$PS_ACCOUNTS_VERSION"
-sleep 6
+sleep 8
 
 #Run the tests
 npx playwright test --project="Account TNR V8" module_installation || true
