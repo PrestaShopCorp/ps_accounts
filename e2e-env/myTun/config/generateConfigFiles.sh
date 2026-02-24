25de5d394a322bb186b5e8812f12b1f4405ba4aa
ervinpresta <114155817+ervinpresta@users.noreply.github.com>
2026-02-24 17:04:21 +0100

    E2e fix tnr (#621)
    * change profile in generateConfigFiles
    
    * comment slack notifications for tests

--- e2e-env/myTun/config/generateConfigFiles.sh
+++ e2e-env/myTun/config/generateConfigFiles.sh
@@ -20,7 +20,8 @@ echo "credentials.json file created !"
 ############ Create config.yml file ###########
 touch "./myTun/config/mytun-config.yml"
 
-config_yaml_content="tunnel: \"$TUNNEL_ID\"
+if [ "$PROFILE" = "multistore" ]; then
+  config_yaml_content="tunnel: \"$TUNNEL_ID\"
 credentials-file: /credentials.json
 ingress:
   - hostname: \"$PS_DOMAIN\"
@@ -28,6 +29,25 @@ ingress:
     originRequest:
       httpHostHeader: \"$PS_DOMAIN\"
   - service: http_status:404"
+elif [ "$PROFILE" = "imageoff" ]; then
+  config_yaml_content="tunnel: \"$TUNNEL_ID\"
+credentials-file: /credentials.json
+ingress:
+  - hostname: \"$PS_DOMAIN\"
+    service: http://shop1:80
+    originRequest:
+      httpHostHeader: \"$PS_DOMAIN\"
+  - service: http_status:404"
+else
+  config_yaml_content="tunnel: \"$TUNNEL_ID\"
+credentials-file: /credentials.json
+ingress:
+  - hostname: \"$PS_DOMAIN\"
+    service: http://prestashop:80
+    originRequest:
+      httpHostHeader: \"$PS_DOMAIN\"
+  - service: http_status:404"
+fi
 # Print in file
 echo "$config_yaml_content" > "./myTun/config/mytun-config.yml"
 echo "config.yml file created !"
