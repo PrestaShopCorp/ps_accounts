4d49ec533b08dacf40ac43faaaba1a4d5eb5ac11
ervinpresta <ervin.klicic@prestashop.com>
2026-04-23 12:25:34 +0200

    change close popup

--- e2e/fixtures/goToModuleManagerPage.fixture.ts
+++ e2e/fixtures/goToModuleManagerPage.fixture.ts
@@ -5,13 +5,13 @@ import {PageManager} from '~/pages/managerPage';
 export const gotToModuleManagerPage = loginFixture.extend<MyFixtures>({
   gotToModuleManagerPage: async ({loginFixture, page}, use) => {
     const pm = new PageManager(loginFixture);
+    if (await pm.fromDashboardPage().isPopupVisible()) {
+      await pm.fromDashboardPage().closePopup();
+    }
     if (await pm.fromDashboardPage().getShopVersion()) {
       await pm.fromBasePage().goToModulesManagerOldPsVersion();
     } else {
       await pm.fromBasePage().goToSubMenu(pm.fromBasePage().modulesParentLink, pm.fromBasePage().moduleManagerLink);
-    }
-    if (await pm.fromDashboardPage().isPopupVisible()) {
-      await pm.fromDashboardPage().closePopup();
     }
     await use(page);
   }
