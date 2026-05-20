11991c946a87049ee5e87abe9ddeb8b2e4c7e978
Sullivan <sullivan.monteiro@prestashop.com>
2026-05-20 15:51:43 +0200

    refactor(upgrade): remove ConfigurationRepository usage and migrate identities

--- upgrade/upgrade-8.0.14.php
+++ upgrade/upgrade-8.0.14.php
@@ -12,9 +12,9 @@ use PrestaShop\Module\PsAccounts\Repository\ConfigurationRepository;
  */
 function upgrade_module_8_0_14($module)
 {
-    /** @var ConfigurationRepository $configurationRepository */
-    $configurationRepository = $module->getService(ConfigurationRepository::class);
-    $configurationRepository->fixMultiShopConfig(true);
+    require_once __DIR__ . '/helpers.php';
+
+    migrate_or_create_identities_v8($module);
 
     return true;
 }
